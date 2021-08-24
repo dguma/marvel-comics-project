5a624e044ce63f34bb524b2ab77b62bce9bc49ad
dguma <dylan.guma@gmail.com>
2021-08-24 10:34:14 -0400

    added button on homepage

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import AllCharacters from './AllCharacters';
 
 const StyledSection = styled.div`
     display: flex;
@@ -16,22 +17,53 @@ const StyledSection = styled.div`
     }
 `;
 
+const StyledButton = styled.button`
+    width: 50%;
+    height: 5em;
+    border: none;
+    background-color: rgb(200,0,0);
+    color: white;
+    text-transform: uppercase;
+    margin: 5% 0;
+
+    a {
+        text-decoration: none;
+        color: white;
+        font-size: 1.5em;
+    }
+
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
 function Characters({charData, url}) {
 
     return (
-        <StyledSection>
-            {
-                charData.slice(0, 9).map(data => {
-                    return (
-                        <div key={data.thumbnail.substr(44)} id={data.id}>
-                            <Link to={`/character-details/${data.thumbnail.substr(44)}`} >
-                                <img src={data.thumbnail} />
-                            </Link>
-                        </div>
-                    );
-                })
-            }
-        </StyledSection>
+        <Fragment>
+            <StyledSection>
+                {
+                    charData.slice(0, 9).map(data => {
+                        return (
+                            <div key={data.thumbnail.substr(44)} id={data.id}>
+                                <Link to={`/character-details/${data.thumbnail.substr(44)}`} >
+                                    <img src={data.thumbnail} />
+                                </Link>
+                            </div>
+                        );
+                    })
+                }
+            </StyledSection>
+            <ButtonContainer>
+                <StyledButton>
+                    <Link to='/characters'>
+                        All Characters
+                    </Link>
+                </StyledButton>
+            </ButtonContainer>
+        </Fragment>
     );
 }
 
